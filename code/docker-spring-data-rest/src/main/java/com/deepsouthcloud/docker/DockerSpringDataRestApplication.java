6d9361881d9f8706d15d7ed30e38d1602e38bdc0
Matt Stine <mstine@gopivotal.com>
2015-11-30 19:51:01 -0500

    move from NFJS 2015 repo for RWX/CDX

---
+++ code/docker-spring-data-rest/src/main/java/com/deepsouthcloud/docker/DockerSpringDataRestApplication.java
@@ -0,0 +1,19 @@
+package com.deepsouthcloud.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+// tag::language_bootstrap[]
+@SpringBootApplication
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
+public class DockerSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringDataRestApplication.class, args);
+    }
+}
+// end::language_bootstrap[]
---
+++ code/docker-spring-data-rest/src/main/java/com/deepsouthcloud/docker/languages/Language.java
@@ -0,0 +1,45 @@
+package com.deepsouthcloud.docker.languages;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// tag::language_entity[]
+@Entity
+@Table(name = "languages")
+public class Language {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String creator;
+
+    // ...
+// end::language_entity[]
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+}
